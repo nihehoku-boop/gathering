bb689da50789108f190703ccfedd5189cd8a8dfc
Nico Hennecke <nihehoku@gmail.com>
2026-01-02 18:01:06 +0100

    fix: correct bitwise mask in AccentColorLoader

--- components/AccentColorLoader.tsx
+++ components/AccentColorLoader.tsx
@@ -8,7 +8,7 @@ const adjustBrightness = (color: string, percent: number) => {
   const amt = Math.round(2.55 * percent)
   const R = Math.max(0, Math.min(255, (num >> 16) + amt))
   const G = Math.max(0, Math.min(255, ((num >> 8) & 0x00FF) + amt))
-  const B = Math.max(0, Math.min(255, (num & 0x000FF) + amt))
+  const B = Math.max(0, Math.min(255, (num & 0x00FF) + amt))
   return '#' + (0x1000000 + R * 0x10000 + G * 0x100 + B).toString(16).slice(1)
 }
 
